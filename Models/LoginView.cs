901eecf7c1a54a7d29915bc418261bc3e786629e
levimarkcode <levimarkmuigai@gmail.com>
2025-01-23 19:17:16 +0300

    Log-in

---
+++ Models/LoginView.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using PipperChat.Models;
+using PipperChat.Data;
+using System.ComponentModel.DataAnnotations;
+
+ public  class   LoginViews
+    {
+        [Required(ErrorMessage =    "Email  is  required")]
+        [EmailAddress(ErrorMessage =    "Invalid    email   address")]
+        public  string  Email   {   get;    set;    }
+
+        [Required(ErrorMessage =    "Password   is  required")]
+        [DataType(DataType.Password)]
+        public  string  Password    {   get;    set;}
+
+        [Display(Name = "Remember me?")]
+        public  bool    RememberMe  {   get;    set;    }
+
+    }
--- obj/Debug/net8.0/PipperChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/PipperChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PipperChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d67d1c8dd84d19bb5af1216539205c803afee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+161334e677f249254886a29a4df02e7a57d5b3fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PipperChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PipperChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
